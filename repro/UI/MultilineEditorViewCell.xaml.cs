83e76076bca0498e3865e26718cd8395a3b3e88e
Andreas Wäscher <awaescher@sodacore.net>
2020-06-08 23:20:38 +0200

    Clean multiline text editor

--- repro/UI/MultilineEditorViewCell.xaml.cs
+++ repro/UI/MultilineEditorViewCell.xaml.cs
@@ -24,22 +24,5 @@ namespace repro.UI
 			get => (string)GetValue(TextProperty);
 			set => SetValue(TextProperty, value);
 		}
-
-		void Editor_MeasureInvalidated(System.Object sender, System.EventArgs e)
-		{
-			if (sender is Editor editor && !string.IsNullOrWhiteSpace(editor.Text))
-				editor.HeightRequest = -1;
-
-			//var tableView = this.Parent as TableView;
-
-			//if (tableView == null || editor == null)
-			//	return;
-
-
-			//// TODO got to try a renderer (wihtout HeightRequest and stuff) ...
-			//// https://medium.com/@georgetsifrikas/embedding-uitextview-inside-uitableviewcell-9a28794daf01
-			//tableView.BatchBegin();
-			//tableView.BatchCommit();
-		}
 	}
 }
